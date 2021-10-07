9f724958c5747432be946db8590d9d24001811bf
Valentin C <valentin.craciun99@gmail.com>
2021-10-07 10:10:57 +0300

    add utils functions

--- tools/utils.go
+++ tools/utils.go
@@ -1,17 +1,32 @@
 package tools
 
 import (
+	"bytes"
+	"crypto/ed25519"
+	"crypto/elliptic"
+	"encoding/binary"
+	"encoding/hex"
+	"errors"
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/ontio/ontology-crypto/ec"
+	"github.com/ontio/ontology-crypto/keypair"
+	"github.com/ontio/ontology-crypto/sm2"
 	"github.com/polynetwork/poly/common"
+	"math/big"
+	"strings"
 )
+
+func EncodeBigInt(b *big.Int) string {
+	if b.Uint64() == 0 {
+		return "00"
+	}
+	return hex.EncodeToString(b.Bytes())
+}
 
 func ParseAuditpath(path []byte) ([]byte, []byte, [][32]byte, error) {
 	source := common.NewZeroCopySource(path)
-	/*
-		l, eof := source.NextUint64()
-		if eof {
-			return nil, nil, nil, nil
-		}
-	*/
+
 	value, eof := source.NextVarBytes()
 	if eof {
 		return nil, nil, nil, nil
@@ -38,6 +53,81 @@ func ParseAuditpath(path []byte) ([]byte, []byte, [][32]byte, error) {
 	return value, pos, hashs, nil
 }
 
+func GetNoCompressKey(key keypair.PublicKey) []byte {
+	var buf bytes.Buffer
+	switch t := key.(type) {
+	case *ec.PublicKey:
+		switch t.Algorithm {
+		case ec.ECDSA:
+			// Take P-256 as a special case
+			if t.Params().Name == elliptic.P256().Params().Name {
+				return ec.EncodePublicKey(t.PublicKey, false)
+			}
+			buf.WriteByte(byte(0x12))
+		case ec.SM2:
+			buf.WriteByte(byte(0x13))
+		}
+		label, err := GetCurveLabel(t.Curve.Params().Name)
+		if err != nil {
+			panic(err)
+		}
+		buf.WriteByte(label)
+		buf.Write(ec.EncodePublicKey(t.PublicKey, false))
+	case ed25519.PublicKey:
+		panic("err")
+	default:
+		panic("err")
+	}
+	return buf.Bytes()
+}
+
+func GetCurveLabel(name string) (byte, error) {
+	switch strings.ToUpper(name) {
+	case strings.ToUpper(elliptic.P224().Params().Name):
+		return 1, nil
+	case strings.ToUpper(elliptic.P256().Params().Name):
+		return 2, nil
+	case strings.ToUpper(elliptic.P384().Params().Name):
+		return 3, nil
+	case strings.ToUpper(elliptic.P521().Params().Name):
+		return 4, nil
+	case strings.ToUpper(sm2.SM2P256V1().Params().Name):
+		return 20, nil
+	case strings.ToUpper(btcec.S256().Name):
+		return 5, nil
+	default:
+		panic("err")
+	}
+}
+
 func ToElrondAddress(addr []byte) string {
-	return ""
+	return hex.EncodeToString(addr)
+}
+
+func Uint64ToBytes(number uint64) []byte {
+	buffer := make([]byte, 8)
+	binary.BigEndian.PutUint64(buffer, number)
+
+	return buffer
+}
+
+func VerifySecp256k1(key []byte, msg []byte, sig []byte) error {
+	pubKey, err := btcec.ParsePubKey(key, btcec.S256())
+	if err != nil {
+		return err
+	}
+
+	signature, err := btcec.ParseSignature(sig, btcec.S256())
+	if err != nil {
+		return err
+	}
+
+	messageHash := chainhash.DoubleHashB(msg)
+	verified := signature.Verify(messageHash, pubKey)
+
+	if !verified {
+		return errors.New("signing.ErrInvalidSignature")
+	}
+
+	return nil
 }
